3e6afb714da7026bc62f68c0d5da34c3c1763a6f
Josh Bello <Jb230913@live.com>
2026-01-15 21:22:39 +0000

    update - added dummy logic for middleware class

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobListingController;
+use App\Http\Controllers\JobListingsEmployer;
+use App\Http\Controllers\JobListingsAdmin;
+use App\Http\Controllers\JobListingsUser;
+use App\Http\Controllers\ApplicationController;
+use App\Http\Controllers\SavedJobListingController;
+use App\Http\Middleware\AuthUser;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', [JobListingController::class, 'index']);
+//->middleware(AuthUser::class, 'handle');
+Route::resource('job-listings', JobListingController::class);
+
+Route::resource('/employer', JobListingsEmployer::class);
+Route::resource('/admin', JobListingsAdmin::class);
+Route::resource('/application', ApplicationController::class);
+Route::resource('/user', JobListingsUser::class);
+Route::resource('/saved-job', SavedJobListingController::class);
