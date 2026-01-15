9f9f2a70c44370ff020d3e2c673d0a1e16b6d427
Josh Bello <Jb230913@live.com>
2026-01-15 21:51:32 +0000

    update - added 1 more class for checkAdmin user

--- app/Http/Middleware/AuthUser.php
+++ app/Http/Middleware/AuthUser.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AuthUser
 {
@@ -15,6 +16,21 @@ class AuthUser
      */
     public function handle(Request $request, Closure $next): Response
     {
+
+        if (!auth()->check() || !auth()->user()->isEmployer()) {
+            abort(403, 'Only employers can access this resource.');
+        }
+
+        //dd("middleware");
         return $next($request);
+    }
+
+
+    public function checkAdmin(Request $request, Closure $next): Response
+    {
+
+        if (!auth()->check() || !auth()->user()->isAdmin()) {
+            abort(403, 'Admin access only.');
+        }
     }
 }
