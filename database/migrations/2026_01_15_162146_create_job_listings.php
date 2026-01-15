1fd3d90d99ace532dfc76d2417c9d52154c50fde
Josh Bello <Jb230913@live.com>
2026-01-15 16:25:59 +0000

    update - created new migration file for job rename job_listings

--- database/migrations/2026_01_15_162146_create_job_listings.php
+++ database/migrations/2026_01_15_162146_create_job_listings.php
@@ -4,17 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
-        Schema::create('jobs', function (Blueprint $table) {
+        Schema::create('job_listings', function (Blueprint $table) {
             $table->id();
 
-            // Employer who posted the job
+            // Employer
             $table->foreignId('user_id')
                 ->constrained()
                 ->cascadeOnDelete();
@@ -24,12 +20,12 @@ return new class extends Migration
                 ->cascadeOnDelete();
 
             $table->string('title');
+            $table->string('slug')->unique();
             $table->text('description');
-            $table->text('description');
+            $table->text('skillset_About');
             $table->text('benefits');
             $table->string('location');
 
-            // Job type
             $table->enum('job_type', [
                 'full-time',
                 'part-time',
@@ -40,20 +36,15 @@ return new class extends Migration
             $table->integer('salary_min')->nullable();
             $table->integer('salary_max')->nullable();
 
-            // Job status
             $table->enum('status', ['open', 'closed'])->default('open');
-
             $table->date('expires_at')->nullable();
 
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('jobs');
+        Schema::dropIfExists('job_listings');
     }
 };
