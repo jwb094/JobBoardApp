7aea073336bfbf3cc14d22058948b3eef418f32e
Josh Bello <Jb230913@live.com>
2026-01-19 20:34:22 +0000

    update - logic added to functions and functions added for logout and display pages for login/register & dashboard

--- app/Http/Controllers/JobListingsUser.php
+++ app/Http/Controllers/JobListingsUser.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\JobListingsUser as JLUser;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
 
 class JobListingsUser extends Controller
 {
@@ -18,18 +21,50 @@ class JobListingsUser extends Controller
      */
     public function index()
     {
+        $user = [];
         //
-        //$user = $this->jobListingsUsers::findOrFail()
-
-        return view('user.dashboard');
+        if (auth()->user()) {
+            $user = auth()->user();
+        }
+        //dd($user);
+        return view('user.dashboard', ['user' => $user]);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    // public function create()
+    // {
+    //     //
+    // }
+
+
+    public function login(Request $request)
     {
-        //
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+        $credentials = $request->only('email', 'password');
+
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended(route('user.dashboard'))->with('success', "You have successfully logged in");
+        }
+
+        return  redirect('/user/signin')->with('status', true)->with('message', "Registration successfully");;
+    }
+
+    /**
+     * Logout
+     * 
+     */
+    public function logOut()
+    {
+        Session::flush();
+        Auth::logout();
+
+        return  redirect(route('home'));
     }
 
     /**
@@ -40,27 +75,39 @@ class JobListingsUser extends Controller
         //
 
         $data = $request->validate([
-            'first_name'   => 'required|exists:categories,id',
-            'last_name'         => 'required|string|max:255',
+            'first_name'   => 'required|',
+            'last_name'    => 'required|string|max:255',
             'email'   => 'required',
             'password' => 'required',
         ]);
 
-        $data['user_id'] = auth()->id();
-        $data['slug'] = Str::slug($data['title']);
+        // $data['user_id'] = auth()->id();
+        // $data['slug'] = Str::slug($data['title']);
+        $data['password'] = Hash::make($request->password);
+        $data['role'] = 'applicant';
+        //dd($data);
 
+        $newUser = $this->JobListingsUser::create($data);
 
-        $this->JobListingsUser::create($data);
+        if (!$newUser->id) {
+            return  redirect('/user/register')->wih('status', false)->with('message', "Registration failed, try again please");;
+        }
+        return  redirect('/user/signin')->with('status', true)->with('message', "Registration successfully");;
     }
 
     /**
-     * Display the specified resource.
+     * Sign in Page
      */
-    public function show(string $id)
+    public function signin()
     {
-        //
+        return view('user.login');
+    }
 
-        return view('user.show');
+
+    // Register PAge
+    public function register()
+    {
+        return view('user.register');
     }
 
     /**
@@ -88,8 +135,8 @@ class JobListingsUser extends Controller
     {
         //
         $user = $this->JobListingsUser::find($id);
+        //delete application from 
         $user->delete();
-        //need to delete session
         return view('home');
     }
 }
