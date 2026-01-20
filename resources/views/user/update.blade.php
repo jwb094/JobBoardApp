00c928ddab76fa7a8ea7d8b81733cda8b6b4f62e
Josh Bello <Jb230913@live.com>
2026-01-20 18:28:16 +0000

    added - html content and laravel syntax for display user data for form

--- resources/views/user/update.blade.php
+++ resources/views/user/update.blade.php
@@ -1,1 +1,56 @@
+<x-layout>
+    @section('title',$user->first_name ." ".$user->last_name ."Applicant")
+    @section('content')
 
+    <div class="bg-white ">
+
+
+        @if ($errors->any())
+        <div class="mb-4 text-red-600">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+        <div class="flex h-screen justify-center items-center relative isolate h-lvh   px-6  lg:px-8">
+            <div class="w-96">
+                <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-black">New Applicant REgisteration</h2>
+                <form action="/user/update/{{ $user->id }}" method="POST">
+                    @csrf
+                    <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                        <div class="sm:col-span-2">
+                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">First Name</label>
+                            <input value="{{ $user->first_name }}" type="text" name="first_name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type your first name" required="">
+                        </div>
+                        <div class="sm:col-span-2">
+                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Last Name</label>
+                            <input value="{{ $user->last_name }}" type="text" name="last_name" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type your last name" required="">
+                        </div>
+                        <div class="sm:col-span-2">
+                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Email </label>
+                            <input value="{{ $user->email }}" type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type your email address" required="">
+                        </div>
+                        <div class="sm:col-span-2">
+                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Password</label>
+                            <input value="{{ $user->password }}" type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type your password" required="">
+                        </div>
+
+
+                        <button type="submit" class="w-96 | mt-6 | text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5">
+                            Update
+                        </button>
+
+                </form>
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+    @endsection
+
+</x-layout>
