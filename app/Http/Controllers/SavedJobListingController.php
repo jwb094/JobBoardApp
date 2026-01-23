3e6c7239b6589ef820c09305c33978cf1ea576e1
Josh Bello <Jb230913@live.com>
2026-01-23 23:23:25 +0000

    add - function for ajax request remove/add job to saved job collection for user

--- app/Http/Controllers/SavedJobListingController.php
+++ app/Http/Controllers/SavedJobListingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SavedJob;
 use Illuminate\Http\Request;
 
 class SavedJobListingController extends Controller
@@ -9,7 +10,42 @@ class SavedJobListingController extends Controller
     //
 
 
-    public function add() {}
+    public function update(Request $request)
+    {
+
+        $request->validate([
+            'job_id' => 'required|exists:job_listings,id',
+        ]);
+
+        $userId = auth()->id();
+
+
+
+        $savedJobExists = SavedJob::where('user_id', $userId)
+            ->where('job_id', $request->job_id)
+            ->exists();
+
+        if ($savedJobExists) {
+            SavedJob::where('user_id', $userId)
+                ->where('job_id', $request->job_id)
+                ->delete();
+
+            return response()->json([
+                'status' => true,
+                'message' => "removed Job from Saved Jobs List",
+            ]);
+        }
+
+        SavedJob::create([
+            'user_id' => $userId,
+            'job_id' => $request->job_id,
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => "Job added to saved jobs",
+        ]);
+    }
 
 
     public function remove() {}
