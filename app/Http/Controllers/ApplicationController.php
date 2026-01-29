0acc10253ddec3b2810cd5e9eb08124d60c2a0c4
Josh Bello <Jb230913@live.com>
2026-01-29 18:59:07 +0000

    update - added logic for store() function

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -5,16 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\JobListing;
 use App\Models\JobListingsUser;
+use App\Models\Application;
+use App\Http\Requests;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Storage;
 
 class ApplicationController extends Controller
 {
     protected JobListing $jobListing;
     protected JobListingsUser $jobListingsUser;
-    public function __construct(JobListing $jobListingModel, JobListingsUser $jobListingsUserModel)
+
+    protected Application $application;
+    public function __construct(JobListing $jobListingModel, JobListingsUser $jobListingsUserModel, Application $applicationModel)
     {
 
         $this->jobListing = $jobListingModel;
         $this->jobListingsUser = $jobListingsUserModel;
+        $this->application = $applicationModel;
     }
 
     /**
@@ -24,17 +31,11 @@ class ApplicationController extends Controller
     {
         //
     }
-    public function store(Request $request, JobListing $jobListing)
+    public function store(Request $request, $job_id, $userId)
     {
-        if (!auth()->user()->isApplicant()) {
-            abort(403);
-        }
-
-        // $request->validate([
-        //     'resume' => 'required|file|mimes:pdf,doc,docx|max:2048',
-        //     'cover_letter' => 'nullable|string',
-        // ]);
-
+        // if ($this->jobListing->where('job_id',  $job_id)->where('user_id', auth()->id())->exists()) {
+        //     return back()->with('error', 'You already applied.');
+        // }
         // if ($jobListing->applications()
         //     ->where('user_id', auth()->id())
         //     ->exists()
@@ -42,10 +43,32 @@ class ApplicationController extends Controller
         //     return back()->with('error', 'You already applied.');
         // }
 
-        // $resumePath = $request->file('resume')
-        //     ->store('resumes', 'public');
+        //Create a folder for User applicant to store documents
+        $doesPathExists = public_path('uploads/' . auth()->user()->first_name . '-' . auth()->user()->last_name);
 
-        // Application::create([
+        $path = "";
+        if (Storage::exists($doesPathExists)) {
+            $path =  $doesPathExists;
+        }
+        //dd($path);
+
+        $data = $request->validate([
+            'resume_path' => 'required',
+            'cover_letter' => 'required',
+        ]);
+
+        // dd($request);
+
+        //create datas array for sql query
+        $data['job_id'] = $job_id;
+        $data['user_id'] = $userId;
+        $data['resume_path'] = $path . '/' . $data['resume_path'];
+        $data['cover_letter'] = $path . '/' . $data['cover_letter'];
+        $data['status'] = 'Received/Submitted';
+
+        //dd($data);
+
+        $updatedUserDocuments = $this->application::create($data);
         //     'job_listing_id' => $jobListing->id,
         //     'user_id'        => auth()->id(),
         //     'resume_path'    => $resumePath,
@@ -53,5 +76,12 @@ class ApplicationController extends Controller
         // ]);
 
         // return back()->with('success', 'Application submitted.');
+
+
+        if (!$updatedUserDocuments->id) {
+            return redirect(route('user.documents'))->with('success', false)->with('message', "uploads Documents failed")->with(compact($data));
+        }
+
+        return  redirect(route('user.dashboard'))->with('success', true)->with('message', "documents  uploaded succesfully");
     }
 }
