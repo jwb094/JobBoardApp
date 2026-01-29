0b707bda9937108c401d97e692664afb2b317280
Josh Bello <Jb230913@live.com>
2026-01-29 21:13:37 +0000

    update - change made to column name used as foreign key for job_listing FK

--- database/migrations/2026_01_28_151446_update_foreign_keys_in_applications_table.php
+++ database/migrations/2026_01_28_151446_update_foreign_keys_in_applications_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->dropForeign(['job_id']);
 
             // Add the correct foreign key pointing to job_listings
-            $table->foreign('job_listing_id')
+            $table->foreign('job_id')
                 ->references('id')
                 ->on('job_listings')
                 ->onDelete('cascade');
