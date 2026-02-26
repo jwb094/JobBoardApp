e9691a1c7bf5da95086378f3537ebdcb8ace31cf
Josh Bello <Jb230913@live.com>
2026-02-26 20:06:38 +0000

    update change input from tel to number for salary_min & salary_max

--- resources/views/employer/new_job.blade.php
+++ resources/views/employer/new_job.blade.php
@@ -60,11 +60,11 @@
                                 </div>
                                 <div class="sm:col-span-2">
                                     <label for="salary_min" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Minimum Salary </label>
-                                    <input type="tel" name="salary_min" id="salary_min" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter Minimum Salary Range">
+                                    <input type="number" name="salary_min" id="salary_min" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter Minimum Salary Range">
                                 </div>
                                 <div class="sm:col-span-2">
                                     <label for="salary_max" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Maximum Salary </label>
-                                    <input type="tel" name="salary_max" id="salary_max" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter Maximum Salary Range">
+                                    <input type="number" name="salary_max" id="salary_max" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter Maximum Salary Range">
                                 </div>
                                 <div class="sm:col-span-2">
                                     <label for="job_type" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Job Type </label>
