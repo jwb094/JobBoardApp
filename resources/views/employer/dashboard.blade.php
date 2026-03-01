2ea322aa8ada2a53c084630ea810db6c87ebaffc
Josh Bello <Jb230913@live.com>
2026-03-01 09:20:28 +0000

    update - change content for new job <a> tag

--- resources/views/employer/dashboard.blade.php
+++ resources/views/employer/dashboard.blade.php
@@ -46,7 +46,7 @@
 
 
 
-                            <a href="{{ route('employer.newjobdesc.page') }}" class="text-fg-brand hover:underline font-medium text-lg inline-flex items-center">View your saved Jobs
+                            <a href="{{ route('employer.newjobdesc.page') }}" class="text-fg-brand hover:underline font-medium text-lg inline-flex items-center">Create a new posting
                                 <svg class="w-6 h-6 rtl:rotate-180 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4" /></svg>
                             </a>
