3bc6ea3c685c1d1cb6c98e1547b122c79aaaeedd
Josh Bello <Jb230913@live.com>
2026-03-04 10:21:56 +0000

    update - removal of commented out html + blade directives + html commets for blade dirtectives for custom styles + scripts directives

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,11 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title','Password Keeper')</title>
-    <!-- <link href="https://cdn.jsdelivr.net/npm/uniformcss@1.0.0/dist/uniform.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css" >
-    <script src="https://cdn.jsdelivr.net/npm/tailwind-ui-components@1.0.0/dist/index.min.js"></script> -->
-    {{-- <link rel="stylesheet" href="{{ URL::asset('resources/css/app.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('resources/css/main.css') }}"> --}}
     <link rel="stylesheet" href="{{ asset('assets/style.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/app.css') }}">
     <link href=" {{ URL::asset('css/mycss.css') }}" rel="stylesheet">
@@ -17,6 +12,7 @@
     <link href="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.css" rel="stylesheet" />
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- custom style injection -->
     @stack('styles')
     <!-- Styles / Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -34,4 +30,5 @@
     @include('include.footer')
 </body>
 </html>
+<!-- custom Scripts injection -->
 @stack('other-scripts')
--- resources/views/employer/jobs_and_applicants.blade.php
+++ resources/views/employer/jobs_and_applicants.blade.php
@@ -10,59 +10,49 @@
             @foreach($jobsAndApplicants as $sectionIndex => $jobsAndApplicant)
             <section class="mb-6">
 
-                {{-- <div class="p-4 mb-4 text-sm text-blue-700 bg-blue-100 rounded-lg | flex flex-row justify-between" role="alert">
-                <span class="font-medium">{{ $jobsAndApplicant->title }}</span>
-                <div>
-                    <a class="w-40 px-2 | mt-6 | text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm py-2" href="/employer/edit_job/{{ $jobsAndApplicant->id }}">Edit</a>
-                    <a class="w-40 px-2 | mt-6 | text-white bg-red-500 hover:bg-green-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm py-2" href="/employer/delete_job/{{ $jobsAndApplicant->id }}">Delete</a>
+
+                <div id="accordion-{{ $sectionIndex }}" class="w-full" data-accordion="collapse">
+
+
+                    <h2 id="accordion-heading-{{ $sectionIndex }}">
+                        <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 bg-gray-100 rounded-t-lg hover:bg-gray-200" data-accordion-target="#accordion-body-{{ $sectionIndex }}" aria-expanded="true" aria-controls="accordion-body-{{ $sectionIndex }}">
+                            {{ $jobsAndApplicant->title }}
+                            <a class="w-40 px-2 | | text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm py-2" href="/employer/edit_job/{{ $jobsAndApplicant->id }}">Edit</a>
+                            <a class="w-40 px-2 | | text-white bg-red-800 hover:bg-red-500 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm py-2" href="/employer/delete_job/{{ $jobsAndApplicant->id }}">Delete</a>
+                            <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+
+                            </svg>
+                        </button>
+                    </h2>
+                    <div id="accordion-body-{{ $sectionIndex }}" class="hidden" aria-labelledby="accordion-heading-{{ $sectionIndex }}">
+                        <div class="p-5 font-light border border-gray-200 border-t-0">
+
+                            @if (count($jobsAndApplicant->applications) === 0)
+                            <p>Currently there has been no applicants for this Job Post so far</p>
+                            @endif
+                            @if (count($jobsAndApplicant->applications) > 0)
+                            @foreach($jobsAndApplicant->applications as $applicantIndex => $applicant)
+                            <ul class="flex flex-row">
+                                <li>{{ $applicantIndex + 1 .")" }} {{ $applicant->applicantUsers->first_name }} &nbsp; {{ $applicant->applicantUsers->last_name }}</li>
+                                <li>
+
+                                </li>
+                            </ul>
+
+                            @endforeach
+                            @endif
+
+                        </div>
+                    </div>
                 </div>
-        </div>--}}
 
-        <div id="accordion-{{ $sectionIndex }}" class="w-full" data-accordion="collapse">
-            <!-- Item 1 -->
+            </section>
 
-            <h2 id="accordion-heading-{{ $sectionIndex }}">
-                <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 bg-gray-100 rounded-t-lg hover:bg-gray-200" data-accordion-target="#accordion-body-{{ $sectionIndex }}" aria-expanded="true" aria-controls="accordion-body-{{ $sectionIndex }}">
-                    {{ $jobsAndApplicant->title }}
-                    <a class="w-40 px-2 | | text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm py-2" href="/employer/edit_job/{{ $jobsAndApplicant->id }}">Edit</a>
-                    <a class="w-40 px-2 | | text-white bg-red-800 hover:bg-red-500 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm py-2" href="/employer/delete_job/{{ $jobsAndApplicant->id }}">Delete</a>
-                    <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-
-                    </svg>
-                </button>
-            </h2>
-            <div id="accordion-body-{{ $sectionIndex }}" class="hidden" aria-labelledby="accordion-heading-{{ $sectionIndex }}">
-                <div class="p-5 font-light border border-gray-200 border-t-0">
-
-                    @if (count($jobsAndApplicant->applications) === 0)
-                    <p>Currently there has been no applicants for this Job Post so far</p>
-                    @endif
-                    @if (count($jobsAndApplicant->applications) > 0)
-                    @foreach($jobsAndApplicant->applications as $applicantIndex => $applicant)
-                    <ul class="flex flex-row">
-                        <li>{{ $applicantIndex + 1 .")" }} {{ $applicant->applicantUsers->first_name }} &nbsp; {{ $applicant->applicantUsers->last_name }}</li>
-                        <li>
-
-                        </li>
-                    </ul>
-
-                    {{-- {{ $applicant->applicantUsers->first_name }} --}}
+            @endforeach
 
 
-                    @endforeach
-                    @endif
-
-                </div>
-            </div>
         </div>
-
-        </section>
-
-        @endforeach
-
-
-    </div>
     </div>
     @endsection
     @push('other-scripts')
