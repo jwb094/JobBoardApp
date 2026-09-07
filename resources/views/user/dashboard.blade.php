da60725147f39d3022c72c09e410eff14f1d99fb
Josh Bello <Jb230913@live.com>
2026-09-07 20:08:54 +0100

    update - remove hardcoded heading element for card

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -18,9 +18,6 @@
                 </div>
                 <div class="grid md:grid-cols-3 gap-8">
                     <x-dashboard.card class="bg-neutral-secondary-soft border border-default rounded-xl p-8 md:p-12" cardTitle="Applications">
-                        <span class="inline-flex items-center bg-brand-softer text-fg-brand-strong text-xs font-medium py-0.5 rounded-xl">
-                            <h4 class="text-heading text-3xl font-semobild my-4">Applications</h4>
-                        </span>
                         @if ($savedApplicationsCount === 0)
                         <p class=" font-normal text-body mb-4">You curretly haven't applied anywhere get started</p>
                         @endif
@@ -35,10 +32,7 @@
                         @endif
                     </x-dashboard.card>
 
-                    <x-dashboard.card class="bg-neutral-secondary-soft border border-default rounded-xl p-8 md:p-12">
-                        <span class="inline-flex items-center bg-brand-softer  text-fg-brand-strong text-xs font-medium  py-0.5 rounded-sm">
-                            <h4 class="text-heading text-3xl font-semobild my-4">Saved Jobs</h4>
-                        </span>
+                    <x-dashboard.card class="bg-neutral-secondary-soft border border-default rounded-xl p-8 md:p-12" cardTitle="Saved Jobs">
                         @if ($savedJobsCount === 0)
                         <p class=" font-normal text-body mb-4">You curretly haven't applied anywhere get started</p>
                         @endif
@@ -52,10 +46,7 @@
                         </a>
                         @endif
                     </x-dashboard.card>
-                    <x-dashboard.card class="bg-neutral-secondary-soft border border-default rounded-xl p-8 md:p-12 flex flex-col">
-                        <span class="inline-flex items-center bg-brand-softer  text-fg-brand-strong text-xs font-medium py-0.5 rounded-sm">
-                            <h4 class="text-heading text-3xl font-semobild my-4"> Your documents</h4>
-                        </span>
+                    <x-dashboard.card class="bg-neutral-secondary-soft border border-default rounded-xl p-8 md:p-12 flex flex-col" cardTitle="Your documents">
                         <a href="/user/{{ $user->id }}/documents" class="text-fg-brand hover:underline font-medium text-lg inline-flex items-center">Read more
                             <svg class="w-6 h-6 rtl:rotate-180 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4" /></svg>
