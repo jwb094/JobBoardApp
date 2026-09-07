71b5d6a92c7463670b195e0e9dd1191f66c3ef0c
Josh Bello <Jb230913@live.com>
2026-09-07 23:38:35 +0100

    update - update(), dashboard(),deleteUser() logic updated

--- app/Services/UserAuthService.php
+++ app/Services/UserAuthService.php
@@ -1,24 +1,70 @@
 <?php
 
 namespace App\Services;
+
+use App\Models\Application;
+use App\Models\JobListingsUser;
+use App\Models\SavedJob;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
-class UserAuthService{
+class UserAuthService
+{
 
-    public function register(){
-        
+    public function register(array $userData): JobListingsUser
+    {
+        $userData['password'] = Hash::make($userData['password']);
+        $userData['role'] = 'applicant';
+
+        //dd($userData);
+        $user = JobListingsUser::create($userData);
+
+        return $user;
     }
 
-    public function login(array $loginDetails){
 
-     return Auth::attempt([
-        'email' => $loginDetails['email'],
-        'password' => $loginDetails['password']
-     ]);
+    public function login(array $loginDetails): bool
+    {
+
+        return Auth::attempt([
+            'email' => $loginDetails['email'],
+            'password' => $loginDetails['password']
+        ]);
     }
 
-    public function logout(){
-        
+
+    public function update(array $updatedUserData, int $updatedUserDataId): JobListingsUser
+    {
+
+        $updatedUser = JobListingsUser::findOrFail($updatedUserDataId);
+
+        $updatedUserData['password'] = Hash::make($updatedUserData['password']);
+        $updatedUserData['role'] = 'applicant';
+
+        $updatedUser->update($updatedUserData);
+
+        return $updatedUser->refresh();
     }
 
+
+    public function dashboard(object $applicantUser)
+    {
+        return [
+            "userSavedJobsCount" =>  SavedJob::where('user_id', '=', $applicantUser->id)->count(),
+            "userApplicationsCount" =>  Application::where('user_id', '=', $applicantUser->id)->count()
+        ];
+    }
+
+
+    public function deleteUser($id)
+    {
+
+        DB::transaction(function () use ($id) {
+
+            Application::where('user_id', $id)->delete();
+
+            JobListingsUser::findOrFail($id)->delete();
+        });
+    }
 }
