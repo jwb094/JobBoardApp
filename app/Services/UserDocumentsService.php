acedc98d780f7d968b64c6fbf26f45ad4aad8a8d
Josh Bello <Jb230913@live.com>
2026-09-07 22:54:21 +0100

    add - new Service File for uploading Docs

---
+++ app/Services/UserDocumentsService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\JobListingsUser;
+use Illuminate\Support\Facades\Storage;
+
+class UserDocumentsService
+{
+
+
+    public function uploadDocuments(object $files, string $user_first_name, string $user_last_name, int $user_id)
+    {   
+        $path = public_path('uploads/' . $user_first_name . '-' . $user_last_name);
+
+
+        if (!Storage::exists($path)) {
+            Storage::makeDirectory($path, 0777, true, true);
+        }
+
+        $cover_letter = $files->file('cover_letter');
+        $cv = $files->file('cv');
+
+        $files->cover_letter->move($path, $cover_letter->getClientOriginalName());
+        $files->cv->move($path, $cv->getClientOriginalName());
+
+
+        $data['cover_letter'] = $cover_letter->getClientOriginalName();
+        $data['cv'] = $cv->getClientOriginalName();
+
+        $updatedUserDocuments =   JobListingsUser::where('id', $user_id)->update($data);
+
+        return $updatedUserDocuments;
+    }
+}
