035e52b7065649c0cc6ebebe59e77bd5486464c0
Josh Bello <Jb230913@live.com>
2026-09-07 21:17:38 +0100

    update. -  edit page url changed from hardcoded to route name prefix

--- resources/views/employer/dashboard.blade.php
+++ resources/views/employer/dashboard.blade.php
@@ -10,7 +10,7 @@
             <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
                 <div class="bg-neutral-secondary-soft border border-default rounded-base p-8 md:p-12 mb-8">
                     <h1 class="text-heading tracking-tighter text-3xl md:text-5xl font-bold my-6"> Personal Details</h1>
-                    <a href="/employer/edit/{{ $user->id }}" type="button" class="inline-flex items-center justify-center text-black bg-brand hover:bg-brand-strong box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium rounded-base text-base px-5 py-3 focus:outline-none">
+                    <a href={{ route('employer.profile.page',$user->id) }} type="button" class="inline-flex items-center justify-center text-black bg-brand hover:bg-brand-strong box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium rounded-base text-base px-5 py-3 focus:outline-none">
                         Update your Personal Details
                         <svg class="w-4 h-4 ms-1.5 -me-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4" /></svg>
