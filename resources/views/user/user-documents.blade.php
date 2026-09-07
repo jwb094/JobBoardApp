23dd106df80b6182f2e8a542e6515493c76636b3
Josh Bello <Jb230913@live.com>
2026-09-08 00:08:34 +0100

    update - switch hard coded form input to component

--- resources/views/user/user-documents.blade.php
+++ resources/views/user/user-documents.blade.php
@@ -21,23 +21,27 @@
                 @csrf
                 <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
                     <div class="sm:col-span-2">
-                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Cover Letter</label>
-                        <input value="{{ $user->cover_letter }}" type="file" name="cover_letter" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                        <x-form.label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Cover Letter</x-form.label>
+                        <x-form.form-input value="{{ $user->cover_letter }}" type="file" name="portfolio_link" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="enter your portfolio link"></x-form.form-input>
+                        <x-info-box role="alert" class="mt-3 relative flex w-full p-3 text-sm text-white bg-slate-800 rounded-md"> Current CV :{{ $user->cover_letter }} </x-info-box>
                     </div>
                     <div class="sm:col-span-2">
-                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">CV</label>
-                        <input value="{{ $user->cv }}" type="file" name="cv" id="cv" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                        <x-form.label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">CV</x-form.label>
+                        <x-form.form-input value="{{ $user->cv }}" type="file" name="portfolio_link" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="enter your portfolio link"></x-form.form-input>
+                        <x-info-box role="alert" class="mt-3 relative flex w-full p-3 text-sm text-white bg-slate-800 rounded-md"> Current Cover Letter : {{ $user->cover_letter }} </x-info-box>
                     </div>
-                    <div class="sm:col-span-2">
-                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Portfolio Link </label>
-                        <input value="{{ $user->portfolio_link }}" type="text" name="portfolio_link" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="enter your portfolio link">
-                    </div>
+                </div>
+                <div class="sm:col-span-2 mt-6">
+                    <x-form.label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Portfolio Link</x-form.label>
+                    <x-form.form-input type="text" name="portfolio_link" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="enter your portfolio link"></x-form.form-input>
+                    <x-info-box role="alert" class="mt-3 relative flex w-full p-3 text-sm text-white bg-slate-800 rounded-md"> Current Cover Letter : {{ $user->portfolio_link ?? "N/A" }} </x-info-box>
+                </div>
 
 
 
-                    <button type="submit" class="w-96 | mt-6 | text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5">
-                        Add
-                    </button>
+                <button type="submit" class="w-96 | mt-6 | text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5">
+                    Add
+                </button>
 
             </form>
         </div>
