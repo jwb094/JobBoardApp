40f897e90f8a32ff17998e9bb554c68675e87abe
Josh Bello <Jb230913@live.com>
2026-09-07 20:05:09 +0100

    update  - insert heading element

--- resources/views/components/dashboard/card.blade.php
+++ resources/views/components/dashboard/card.blade.php
@@ -1,5 +1,9 @@
 @props(['cardTitle'])
 
 <div {{ $attributes->merge(['id' => '','name' => '','class' => '']) }}>
+    <span class="inline-flex items-center bg-brand-softer text-fg-brand-strong text-xs font-medium  py-0.5 rounded-sm">
+        <h4 class="text-heading text-3xl font-semobild my-4"> {{ $cardTitle }}</h4>
+    </span>
     {{ $slot }}
+
 </div>
