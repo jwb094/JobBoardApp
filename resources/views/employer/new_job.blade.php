36a60411c87c2957e31daef64be5add83a49ceb3
Josh Bello <Jb230913@live.com>
2026-09-08 02:13:33 +0100

    update - all label converted to component elements

--- resources/views/employer/new_job.blade.php
+++ resources/views/employer/new_job.blade.php
@@ -47,11 +47,11 @@
                         <div class="step active">
 
                             <div class="sm:col-span-2">
-                                <label for="job_title" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Job Title</label>
+                                <x-form.label for="job_title" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Maximum Salary </x-form.label>
                                 <input type="text" value="{{ old('title') }}" name="title" id="job_title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type your company name">
                             </div>
                             <div class="sm:col-span-2">
-                                <label for="job_category" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Job Status </label>
+                                <x-form.label for="job_category" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Category </x-form.label>
                                 <select name="category_id" id="job_category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500">
                                     @foreach ($categories as $category)
                                     <option value="{{ $category->id }}">{{ $category->name }}</option>
@@ -59,15 +59,16 @@
                                 </select>
                             </div>
                             <div class="sm:col-span-2">
-                                <label for="salary_min" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Minimum Salary </label>
+                                <x-form.label for="salary_min" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Minimum Salary </x-form.label>
                                 <input type="number" value="{{ old('salary_min') }}" name="salary_min" id="salary_min" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter Minimum Salary Range">
                             </div>
                             <div class="sm:col-span-2">
-                                <label for="salary_max" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Maximum Salary </label>
+
+                                <x-form.label for="salary_max" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Maximum Salary </x-form.label>
                                 <input type="number" value="{{ old('salary_max') }}" name="salary_max" id="salary_max" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter Maximum Salary Range">
                             </div>
                             <div class="sm:col-span-2">
-                                <label for="job_type" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Job Type </label>
+                                <x-form.label for="job_type" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Job Type </x-form.label>
                                 <select name="job_type" id="job_type" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500">
                                     <option value="Full-time">Full-time</option>
                                     <option value="Part-time">Part-time</option>
@@ -76,37 +77,37 @@
                                 </select>
                             </div>
                             <div class="sm:col-span-2">
-                                <label for="status" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Job Status </label>
+                                <x-form.label for="status" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Status </x-form.label>
                                 <select name="status" id="status" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500">
                                     <option value="open">Open</option>
                                     <option value="closed">Closed</option>
                                 </select>
                             </div>
                             <div class="sm:col-span-2">
-                                <label for="job_expiry_date" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Closing Date:</label>
+                                <x-form.label for="job_expiry_date" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Closing Date:</x-form.label>
                                 <input type="date" name="expires_at" value="{{ old('expires_at') }}" id="job_expiry_date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Please Enter Company  Post Code">
                             </div>
                         </div>
                         <div class="step">
                             <div class="sm:col-span-2">
-                                <label for="company_Desc" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Company Background Description :</label>
+                                <x-form.label for="company_Desc" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Company Background Description</x-form.label>
                                 <textarea name="company_background_info" value="{{ old('company_background_info') }}" id="company_Desc" class="summernote"></textarea>
                             </div>
                             <div class="sm:col-span-2">
-                                <label for="company_location" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Location :</label>
+                                <x-form.label for="company_location" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Company Location </x-form.label>
                                 <input type="text" value="{{ old('location') }}" name="location" id="company_location" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Please Enter Company Location">
 
                             </div>
                             <div class="sm:col-span-2">
-                                <label for="company_city" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">City :</label>
+                                <x-form.label for="company_city" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Company Location </x-form.label>
                                 <input value="{{ old('company_city') }}" type="text" name="city" id="company_city" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Please Enter Company  City">
                             </div>
                             <div class="sm:col-span-2">
-                                <label for="company_address" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Address:</label>
+                                <x-form.label for="company_address" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Address </x-form.label>
                                 <input type="text" name="address" {{ old('address') }} id="company_address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Please Enter Company  Address">
                             </div>
                             <div class="sm:col-span-2">
-                                <label for="company_post_code" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Post Code:</label>
+                                <x-form.label for="company_address" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Post Code </x-form.label>
                                 <input {{ old('post_code') }} type="text" name="post_code" id="company_post_code" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Please Enter Company  Post Code">
                             </div>
 
@@ -117,19 +118,19 @@
                         </div>
                         <div class="step">
                             <div class="sm:col-span-2">
-                                <label for="company_tel" class="block my-4 text-sm font-medium text-gray-900 dark:text-black">Job Description </label>
+                                <x-form.label for="description" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Job Description </x-form.label>
                                 <textarea name="description" id="description" class="summernote">
-                                    {{ old('description') }}
+                                {{ old('description') }}
                                 </textarea>
                             </div>
                             <div class="sm:col-span-2">
-                                <label for="salary_max" class="block my-4 text-sm font-medium text-gray-900 dark:text-black">Skillset </label>
+                                <x-form.label for="skillset_About" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Skillset </x-form.label>
                                 <textarea name="skillset_About" id="skillset_About" class="summernote">
                                 {{ old('skillset_About') }}
                                 </textarea>
                             </div>
                             <div class="sm:col-span-2">
-                                <label for="salary_max" class="block my-4 text-sm font-medium text-gray-900 dark:text-black">Benefits</label>
+                                <x-form.label for="benefits" class="block my-2 text-sm font-medium text-gray-900 dark:text-black">Benefits </x-form.label>
                                 <textarea name="benefits" id="benefits" class="summernote">{{ old('benefits') }}</textarea>
                             </div>
 
