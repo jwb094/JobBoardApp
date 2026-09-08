5f507833738d92e45c949dca0fc187513d38bd4b
Josh Bello <Jb230913@live.com>
2026-09-08 02:07:01 +0100

    update. - editJob() + register() added

--- app/Services/EmployerService.php
+++ app/Services/EmployerService.php
@@ -5,7 +5,11 @@ namespace App\Services;
 
 use App\Models\Company;
 use App\Models\JobListing;
+use App\Models\JobListingsUser;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+
 class  EmployerService
 {
 
@@ -19,17 +23,19 @@ class  EmployerService
 
 
 
-    public function getJobsAndApplicants(object $user): mixed {
+    public function getJobsAndApplicants(object $user): mixed
+    {
 
         $jobsAndApplicants = JobListing::where('company_id', $user->company_id)
             ->where('status', 'open')
             ->with('applications.applicantUsers')
             ->get();
 
-            return $jobsAndApplicants;
+        return $jobsAndApplicants;
     }
 
-    public function getCompanyDetails(object $user): mixed {
+    public function getCompanyDetails(object $user): mixed
+    {
         $companyName =   Company::where("id", $user->company_id)->select("company_name")->get();
         $companyName = json_decode($companyName, true);
         $companyName = ucfirst($companyName[0]['company_name']);
@@ -37,7 +43,8 @@ class  EmployerService
         return $companyName;
     }
 
-    public function newJob(array $newJobFormData,object $user): mixed {
+    public function newJob(array $newJobFormData, object $user): mixed
+    {
 
         $newJobFormData['user_id']    = $user->id;
         $newJobFormData['company_id'] = $user->company_id;
@@ -46,11 +53,80 @@ class  EmployerService
         $newJobFormData['expires_at'] = strtotime($newJobFormData['expires_at']);
         $newJobFormData['slug']       = Str::slug($newJobFormData['title']) . '-' . rand(1000, 9999);
 
-   
+
         $newJobDesc = JobListing::create($newJobFormData);
         return $newJobDesc;
     }
 
 
-    public function editJob() {}
+    public function editJob(array $editJobFormData,int $jobId ,object $user): mixed
+    {
+
+        $editJobFormData['user_id'] = $user->id;
+        $editJobFormData['salary_min'] = $editJobFormData['salary_min'] ?? '';
+        $editJobFormData['salary_max'] = $editJobFormData['salary_max'] ?? '';
+        $editJobFormData['company_id'] = $user->company_id;
+        $editJobFormData['expires_at'] =  strtotime($editJobFormData['expires_at']);
+
+        $updatedJobDesc = JobListing::where('id', $jobId)->update($editJobFormData);
+        return $updatedJobDesc;
+    }
+
+
+    public function login(){
+
+    }
+
+
+    public function register(array $employerRegisteredDetails):mixed {
+
+       DB::beginTransaction();
+
+        try {
+
+
+            $company = Company::create(
+                ['company_name' => $employerRegisteredDetails['company_name'], 
+                'company_tel' => $employerRegisteredDetails['company_tel'], 
+                'company_size'=> $employerRegisteredDetails['company_size']]
+            );
+
+            $userData = (['first_name' =>$employerRegisteredDetails['first_name'], 
+            'last_name' =>$employerRegisteredDetails['last_name'],
+             'email' =>$employerRegisteredDetails['email'],
+             'password' =>$employerRegisteredDetails['password']]);
+
+            if (!empty($employerRegisteredDetails['password'])) {
+                $userData['password'] = Hash::make($employerRegisteredDetails['password']);
+            }
+
+            $userData['role'] = 'employer';
+            $userData['company_id'] = $company->id;
+
+            JobListingsUser::create($userData);
+
+            DB::commit();
+
+            if($userData && $company){
+                   return [
+                    'success' => true,
+                    'message' => 'new Employer and Company was Registered successfully.'
+                ];
+            }
+
+        } catch (\Exception $e) {
+
+            DB::rollBack();
+
+            return $e;
+        }
+    }
+
+
+    public function updateProfile(){
+
+
+
+    //$updatedUserData =  $this->JobListingsUser->where('id', $id)->update($validated);
+    }
 }
