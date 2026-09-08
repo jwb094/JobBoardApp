4a1c2bc5cc2b4fa541741ba9b0eeb5112019f632
Josh Bello <Jb230913@live.com>
2026-09-08 02:04:44 +0100

    update - refactor update(),store(),update_profile(),login()

--- app/Http/Controllers/JobListingsEmployer.php
+++ app/Http/Controllers/JobListingsEmployer.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CheckSignInUserRequest;
+use App\Http\Requests\CreateEmployerUserRequest;
+use App\Http\Requests\EditJobDescriptionRequest;
 use App\Http\Requests\StoreJobDescriptionRequest;
+use App\Http\Requests\CreateApplicantUserRequest;
 use App\Models\Category;
+use App\Services\UserAuthService;
 use Illuminate\Http\Request;
 use App\Models\JobListingsUser;
 use App\Models\Company;
@@ -28,13 +33,16 @@ class JobListingsEmployer extends Controller
     protected Company $Company;
 
     protected  EmployerService $employerService;
-    public function __construct(Category $categoryModel, JobListingsUser $jobListingsUserModel,  JobListing $jobListingModel, Company $companyModel, EmployerService $employerService)
+
+    protected UserAuthService $userAuthService;
+    public function __construct(Category $categoryModel, JobListingsUser $jobListingsUserModel,  JobListing $jobListingModel, Company $companyModel, EmployerService $employerService,   UserAuthService $userAuthServices)
     {
         $this->JobListingsUser = $jobListingsUserModel;
         $this->Company =  $companyModel;
         $this->JobListing = $jobListingModel;
         $this->Category = $categoryModel;
         $this->employerService = $employerService;
+        $this->userAuthService = $userAuthServices;
     }
 
     /** 
@@ -71,8 +79,6 @@ class JobListingsEmployer extends Controller
 
     public function edit_job(string $id)
     {
-        // $job = $this->JobListing::findOrFail($id);
-        // $categories = $this->Category::all();
 
 
         $job = $this->JobListing
@@ -124,66 +130,13 @@ class JobListingsEmployer extends Controller
         );
     }
 
-    // public function jobDescs(string $id)
-    // {
-    //     //
-    // }
-
-
-
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    // public function create(Request $request)
-    // {
-
-
-
-    //     $validatedJobData =  $request->validate([
-    //         'title'                     => 'required|string',
-    //         // 'slug'                   => 'required|string',
-    //         'description'               => 'required|string',
-    //         'company_background_info'   => 'required|string',
-    //         'skillset_About'            => 'required|string',
-    //         'benefits'                  => 'required|string',
-    //         'location'                  => 'required|string',
-    //         'category_id'               => 'required|exists:categories,id',
-    //         'city'                      => 'required|string',
-    //         'address'                   => 'required|string',
-    //         'post_code'                 => 'required|string',
-    //         'job_type'                  => 'required|string',
-    //         // 'salary_min'                => 'numeric:strict',
-    //         // 'salary_max'                => 'numeric:strict',
-    //         'status'                    => 'required|string',
-    //         'expires_at'                => 'required|string'
-    //     ]);
-
-    //     $validatedJobData['user_id']        = auth()->user()->id;
-    //     $validatedJobData['salary_min']     = $validatedJobData['salary_min'] ?? '';
-    //     $validatedJobData['salary_max']     = $validatedJobData['salary_max'] ?? '';
-
-    //     $validatedJobData['expires_at']     =  strtotime($request->expires_at);
-    //     $validatedJobData['slug']           =  Str::slug($request->title);
-    //     $validatedJobData['company_id']     = $user->company_id;
-
-    //     //dd($validatedJobData);
-    //     $newJobDesc = $this->JobListing::create($validatedJobData);
-
-    //     if (!$newJobDesc->id) {
-    //         return redirect()->intended(route('employer.newjobdesc.page'))->with('status', false)->with('message', "Unsuccessfully created new Job");
-    //     }
-
-    //     return  redirect()->intended(route('employer.dashboard'))->with('status', true)->with('message', "You have successfully created new Job");;
-    // }
-
 
     public function create(StoreJobDescriptionRequest $request)
     {
         $user = auth()->user();
 
         $validatedNewJobDetails = $request->validated();
-        $newJob = $this->employerService->newJob($validatedNewJobDetails,auth()->user());
+        $newJob = $this->employerService->newJob($validatedNewJobDetails, auth()->user());
 
         if (!$newJob->id) {
             return redirect()
@@ -200,38 +153,14 @@ class JobListingsEmployer extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(EditJobDescriptionRequest $request, string $id)
     {
         $user = auth()->user();
 
+        $validatedEditedJobDetails = $request->validated();
+        $editedJob = $this->employerService->editJob($validatedEditedJobDetails, (int) $id, $user);
 
-
-        $validatedUpdatedJobData =  $request->validate([
-            'title'                     => 'required|string',
-            'description'               => 'required|string',
-            'company_background_info'   => 'required|string',
-            'skillset_About'            => 'required|string',
-            'benefits'                  => 'required|string',
-            'location'                  => 'required|string',
-            'category_id'               => 'required|exists:categories,id',
-            'city'                      => 'required|string',
-            'address'                   => 'required|string',
-            'post_code'                 => 'required|string',
-            'job_type'                  => 'required|string',
-            'status'                    => 'required|string',
-            'expires_at'                => 'required|date'
-        ]);
-
-
-        $validatedUpdatedJobData['user_id'] = $user->id;
-        $validatedUpdatedJobData['salary_min'] = $validatedUpdatedJobData['salary_min'] ?? '';
-        $validatedUpdatedJobData['salary_max'] = $validatedUpdatedJobData['salary_max'] ?? '';
-
-        $validatedUpdatedJobData['expires_at'] =  strtotime($request->expires_at);;
-        $validatedUpdatedJobData['company_id'] = $user->company_id;
-
-        $updatedJobDesc = $this->JobListing::where('id', $id)->update($validatedUpdatedJobData);
-        if (!$updatedJobDesc) {
+        if (!$editedJob) {
             return redirect()->intended(route('employer.editjobdesc.page'))->with('status', false)->with('message', "Unsuccessfully  edited" . $request->title . "Job Description");
         }
 
@@ -244,122 +173,36 @@ class JobListingsEmployer extends Controller
 
 
 
-    /**
-     * Store a newly created resource in storage.
-     */
-
-    /*
-    public function store(Request $request)
+    public function store(CreateEmployerUserRequest $request)
     {
-        //dd($request->all());
-        $validated = $request->validate([
-            'first_name'   => 'required|string|max:255',
-            'last_name'    => 'required|string|max:255',
-            'email'         => 'nullable|string',
-            'password'  => 'nullable|string',
-            'company_name'   => 'required|string|max:255',
-            'company_tel'    => 'required|string|max:255',
-            'company_size'    => 'required|string|max:255'
-        ]);
+        $validatedEmployerDetails = $request->validated();
 
-
-
-
-        $company = Company::create(
-            $request->only(['company_name', 'company_tel', 'company_size'])
-        );
-
-        // Prepare user data
-        $userData = $request->only(['first_name', 'last_name', 'email', 'password']);
-        $userData['password'] = isset($validated['password'])
-            ? Hash::make($validated['password'])
-            : null;
-        $userData['role'] = 'employer';
-        $userData['company_id'] = $company->id;
-
-        // Create user
-        $user = JobListingsUser::create($userData);
-
-        if (is_null($user) && is_null($company)) {
-            return  redirect('/employer/register')->wih('status', false)->with('message', "Registration failed, try again please");
-        }
-        return  redirect('/employer/signin')
-            ->with('status', true)
-            ->with('message', "Registration successfully");
-    }
-        */
-
-
-    public function store(Request $request)
-    {
-
-        $validated = $request->validate([
-            'first_name'   => 'required|string|max:255',
-            'last_name'    => 'required|string|max:255',
-            'email'        => 'required|string',
-            'password'     => 'required|string',
-            'company_name' => 'required|string|max:255',
-            'company_tel'  => 'required|string|max:255',
-            'company_size' => 'required|string|max:255'
-        ]);
-
-        DB::beginTransaction();
-
-        try {
-
-            $company = Company::create(
-                $request->only(['company_name', 'company_tel', 'company_size'])
-            );
-
-            $userData = $request->only(['first_name', 'last_name', 'email', 'password']);
-
-            if (!empty($validated['password'])) {
-                $userData['password'] = Hash::make($validated['password']);
-            }
-
-            $userData['role'] = 'employer';
-            $userData['company_id'] = $company->id;
-
-            JobListingsUser::create($userData);
-
-            DB::commit();
-
+        $newEmployer = $this->employerService->register($validatedEmployerDetails);
+        
+        if($newEmployer["success"] === true){
             return redirect('/employer/signin')
                 ->with('status', true)
                 ->with('message', 'Registration successfully');
-        } catch (\Exception $e) {
-
-            DB::rollBack();
-
-            return redirect('/employer/register')
-                ->with('status', false)
-                ->with('message', 'Registration failed, try again');
         }
+
+        return redirect('/employer/register')
+            ->with('status', false)
+            ->with('message', 'Registration failed, try again');
     }
 
-    public function update_profile(Request $request, string $id)
+    public function update_profile(CreateApplicantUserRequest $request, string $id)
     {
-        //dd($request->all());
-        $validated = $request->validate([
-            'first_name'   => 'required|string|max:255',
-            'last_name'    => 'required|string|max:255',
-            'email'        => 'required|string',
-            'password'     => 'required|string',
+        $request->validated();
 
-        ]);
+        $updatedApplicantUser = $this->userAuthService->update($request->validated(), (int)  $id, $type = "employer");
 
-
-
-        $updatedUserData =  $this->JobListingsUser->where('id', $id)->update($validated);
-
-
-        if (!$updatedUserData) {
-            return redirect('/employer/edit/' . $id)
+        if (!$updatedApplicantUser) {
+            return redirect(route('employer.profile.page',$id))
                 ->with('status', true)
                 ->with('message', 'Profile Update unsuccessfully');
         }
 
-        return redirect('/employer/dashboard')
+        return redirect(route('employer.dashboard'))
             ->with('status', true)
             ->with('message', 'Profile Update successfully');
     }
@@ -367,16 +210,14 @@ class JobListingsEmployer extends Controller
 
 
 
-    public function login(Request $request)
+    public function login(CheckSignInUserRequest $request)
     {
-        $request->validate([
-            'email' => 'required',
-            'password' => 'required',
-        ]);
-        $credentials = $request->only('email', 'password');
+        $loginCredentials = $request->validated();
+
+        $authenciated =  $this->userAuthService->login($loginCredentials);
 
 
-        if (Auth::attempt($credentials)) {
+        if ($authenciated) {
             return redirect()->intended(route('employer.dashboard'))->with('success', "You have successfully logged in");
         }
 
