65c9673806649c9cd0dc17ce32e89863b25233da
Josh Bello <Jb230913@live.com>
2026-09-08 01:14:16 +0100

    update - added two missing fields salary_min,salary_max

---
+++ app/Http/Requests/EditJobDescriptionRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EditJobDescriptionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title'                     => 'required|string',
+            'description'               => 'required|string',
+            'company_background_info'   => 'required|string',
+            'skillset_About'            => 'required|string',
+            'benefits'                  => 'required|string',
+            'location'                  => 'required|string',
+            'category_id'               => 'required|exists:categories,id',
+            'city'                      => 'required|string',
+            'address'                   => 'required|string',
+            'salary_min'                => 'nullable|numeric',
+            'salary_max'                => 'nullable|numeric',
+            'post_code'                 => 'required|string',
+            'job_type'                  => 'required|string',
+            'status'                    => 'required|string',
+            'expires_at'                => 'required|date'
+        ];
+    }
+}
